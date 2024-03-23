e82aa778597155a25a5318bd0a3f247f3e0fe153
Yeonjoo Lee <joos0207@naver.com>
2024-03-23 17:59:30 +0900

    주문 조회 V6: JPA에서 DTO로 직접 조회, 플랫 데이터 최적화

--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
@@ -3,11 +3,13 @@ package jpabook.jpashop.repository.order.query;
 import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.OrderStatus;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
+@EqualsAndHashCode(of = "orderId") //v6 api 코드에서 collect의 그룹 바이할 때 orderId를 기준으로 묶어주도록 한다.
 public class OrderQueryDto {
 
     private Long orderId;
@@ -24,5 +26,14 @@ public class OrderQueryDto {
         this.orderStatus = orderStatus;
         this.address = address;
         //orderItems 리스트 제외.
+    }
+
+    public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address, List<OrderItemQueryDto> orderItems) {
+        this.orderId = orderId;
+        this.name = name;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address;
+        this.orderItems = orderItems;
     }
 }
--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
@@ -77,4 +77,15 @@ public class OrderQueryRepository {
     }
 
 
+    public List<OrderFlatDto> findAllByDto_flat() {
+        return em.createQuery(
+                "select new "+
+                        " jpabook.jpashop.repository.order.query.OrderFlatDto(o.id, m.name, o.orderDate, o.status, d.address, i.name, oi.orderPrice, oi.count)" +
+                        " from Order o" +
+                        " join o.member m" +
+                        " join o.delivery d" +
+                        " join o.orderItems oi" + //데이터 뻥튀기 되긴 한다.
+                        " join oi.item i", OrderFlatDto.class)
+                .getResultList();
+    }
 }
